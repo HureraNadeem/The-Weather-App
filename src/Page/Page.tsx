585df8939efd6fc01ea33f65f7cf04287cbb7dd4
Hurera Nadeem <muhammadhureran8@gmail.com>
2022-01-17 17:46:58 +0500

    Initial commit

---
+++ src/Page/Page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Display from "../Components/Display/Display";
+import Search from "../Components/Search/Search";
+import Header from "../Components/Header/Header";
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <Search />
+            <Display />
+        </div>
+    )
+}
+
+export default Page
